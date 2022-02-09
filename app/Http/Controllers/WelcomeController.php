57bb38a2f93ce0be300307b857a2ca430feab25b
Jochem <dijk0146@hz.nl>
2022-02-09 09:26:15 +0100

    braces not on same line

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,7 +9,8 @@ class WelcomeController
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
 
-    public function show() {
+    public function show()
+    {
             return view('welcome');
         }
 }
