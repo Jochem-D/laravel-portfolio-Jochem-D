10f517f9dc2174f0078655d437ec5cae12fe3db0
Jochem <dijk0146@hz.nl>
2022-02-13 17:11:01 +0100

    Finished Week 1

---
+++ app/Models/Grade.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+
+
+    public function addResult($grade): string
+    {
+        if ($grade >= $this->best_grade) {
+            if (($this->best_grade < $this->lowest_passing_grade) && $grade > $this->lowest_passing_grade) {
+                $this->passed_at = now();
+            }
+            $this->best_grade = $grade;
+            $this->save();
+
+            return 'Added new Result...';
+        } else {
+            return 'Previous Result was better';
+        }
+    }
+}
